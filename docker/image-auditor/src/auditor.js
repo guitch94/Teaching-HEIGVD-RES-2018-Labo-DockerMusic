508e186b3c180d28df53dc1cb0d701246736fe28
Guillaume.Blanco <guillaume.blanco@heig-vd.ch>
2018-05-17 22:02:13 +0200

    test4

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,4 +1,4 @@
-/*var dgram = require('dgram');
+var dgram = require('dgram');
 var dgramSocket = dgram.createSocket('udp4');
 var ADRESSE_IP = '239.255.22.19';
 const TIME = 5000;
@@ -38,8 +38,6 @@ dgramSocket.on('message',	function(msg)	{
 
 
 
-
-
 var netTcp = net.createServer();
 
 netTcp.on('connection', function(msg){
@@ -61,53 +59,4 @@ netTcp.on('connection', function(msg){
 
 });
 
-netTcp.listen(PORT_TCP);*/
-
-
-var dgram = require('dgram');
-var net = require('net');
-var moment = require('moment');
-var s = dgram.createSocket('udp4');
-
-var musicians = new Map();
-
-s.on('message', function(msg, source){
-	console.log("received" + msg);
-	var musician = JSON.parse(msg);
-	if(musicians.has(musician.uuid)){
-		musicians.get(musician.uuid).lastUpdate = moment();
-	}else{
-			mus = new Object();
-			mus.uuid = musician.uuid;
-			mus.instrument = musician.instrument;
-			mus.activeSince = moment();
-			mus.lastUpdate = moment();
-			musicians.set(mus.uuid, mus);
-	}
-});
-
-s.on('listening', function(){
-  console.log("listening...");
-});
-
-s.bind(2205, function(){
-	s.addMembership("239.255.22.5");
-});
-
-var server = net.createServer(function(socket) {
-	var arrayToSend = [];
-	musicians.forEach( function(value, key, musicians){
-		console.log("INSIDE FOREACH");
-		if(value.lastUpdate.isAfter(moment().subtract(5, 'seconds'))){
-			var mu = new Object();
-			mu.uuid = key;
-			mu.instrument = value.instrument;
-			mu.activeSince = value.activeSince;
-			arrayToSend.push(mu);
-		}
-	});
-	socket.write(JSON.stringify(arrayToSend)+"\r\n");
-	socket.end();
-});
-
-server.listen(2205);
+netTcp.listen(PORT_TCP);
