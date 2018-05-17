5d3a7ab0eaeeed5ab8a5bcd3f2d15accc3cbe064
Guillaume.Blanco <guillaume.blanco@heig-vd.ch>
2018-05-17 21:22:41 +0200

    test

---
+++ docker/image-auditor/src/auditor.js
@@ -0,0 +1,64 @@
+var dgram = require('dgram');
+var dgramSocket = dgram.createSocket('udp4');
+var ADRESSE_IP = '239.255.22.19';
+const TIME = 5000;
+const PORT = 3205;
+const PORT_TCP = 2205;
+var net = require('net');
+
+var instrument = new Map();
+
+instrument.set("piano", "ti-ta-ti");
+instrument.set("trumpet", "pouet");
+instrument.set("flute", "trulu");
+instrument.set("violin", "gzi-gzi");
+instrument.set("drum", "boum-boum");
+
+var musicianArray = [];
+
+
+dgramSocket.bind(PORT,	function()	{
+    console.log("musician arrive");
+	dgramSocket.addMembership(ADRESSE_IP);
+});
+
+
+dgramSocket.on('message',	function(msg)	{
+
+    var  musician = JSON.parse(msg);
+    for (var i = 0; i < musicianArray.length; i++){
+        if(musician.uuid == musicianArray[i].uuid){
+            musicianArray[i].activeSince = musician.activeSince;
+        }else {
+             musicianArray.push(musician);
+        }
+    }
+});
+
+
+
+
+
+
+var netTcp = net.createServer();
+
+netTcp.on('connection', function(msg){
+
+       musicianArray.forEach(musician => {
+
+            var i = musicianArray.indexOf(musician);
+
+            // if the musician didn't play for a time, we remove it
+            if(moment(new Date()).diff(musician[i].activeSince) > TIME){
+                console.log('This musician is not here for too long\n' + JSON.stringify(musicianArray[i],null,'\t'));
+                musicianArray.splice(i, 1);
+            }
+        })
+
+
+    msg.write(JSON.stringify(musicians,null,'\t') + "\n");
+    msg.end();
+
+});
+
+netTcp.listen(PORT_TCP);
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,9 +1,9 @@
-var uuid = require('uuid/v4');
+const uuidv4 = require('uuid/v4');
 var dgram = require('dgram');
 var dgramSocket = dgram.createSocket('udp4');
-var ADRESSE_IP = '172.100.100.1';
-var TIME = 1000;
-var PORT = 3205;
+var ADRESSE_IP = '239.255.22.19';
+const TIME = 1000;
+const PORT = 3205;
 
 
 var instrument = new Map();
@@ -14,14 +14,18 @@ instrument.set("flute", "trulu");
 instrument.set("violin", "gzi-gzi");
 instrument.set("drum", "boum-boum");
 
+
 var intrumentChoose = process.argv[2];
 
-var message = JSON.stringify({uuid: uuid, sound: instrument.get(intrumentChoose)})
+var uuidAEnvoyer = uuidv4();
+
+
+var message = JSON.stringify({uuid: uuidAEnvoyer, sound: instrument.get(intrumentChoose), activeSince: new Date()});
 
 setInterval(function(){
 
-dgramSocket.send(message,0,message.length,PORT,ADRESSE_IP,function(err,bytes)){
-    console.log("Message send");
+dgramSocket.send(message,0,message.length,PORT,ADRESSE_IP,function(err,bytes){
+    console.log("Message send" + message);
 });
 
 
